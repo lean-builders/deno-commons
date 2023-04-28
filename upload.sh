c6cf770a2d909ee18355a08ed08979f980c4981a
Takeshi Miyakawa <tmiya@leanbuilders.co.jp>
2023-04-28 10:49:22 +0900

    add securityHandlers.ts to upload.sh

--- upload.sh
+++ upload.sh
@@ -1,5 +1,4 @@
 #!/bin/zsh
 
 aws s3 cp narrowCopy.ts s3://leanbuilders-homepage/deno-commons/narrowCopy.ts
-
-
+aws s3 cp securityHandlers.ts s3://leanbuilders-homepage/deno-commons/securityHandlers.ts
